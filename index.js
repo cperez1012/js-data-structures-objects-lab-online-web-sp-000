fa625686417f4f043db78aedbcfabc9a427986c3
Christian Perez <perezchristian1012@gmail.com>
2020-02-18 02:05:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const driver = {};
 
-function updateDriverWithKeyAndValue() {
-
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const obj = { ...driver };
+  obj[key] = value;
+  return obj;
 }
 
-function destructivelyUpdateDriverWithKeyAndValue() {
-
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
 }
 
-function deleteFromDriverByKey() {
-
+function deleteFromDriverByKey(driver, key) {
+  const obj = { ...driver };
+  delete obj[key];
+  return obj;
 }
 
-function destructivelyDeleteFromDriverByKey() {
-
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
 }
